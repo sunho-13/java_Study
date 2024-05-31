fa42f07b8dc6fe4a663a02e08eaea821c5bd7443
sunho13 <sun710101@gmail.com>
2024-05-31 20:49:22 +0900

    Update Exam001JavaTest.java

--- study01/src/test/java/com/studyjavamaven/study01/Exam001JavaTest.java
+++ study01/src/test/java/com/studyjavamaven/study01/Exam001JavaTest.java
@@ -1,8 +1,11 @@
 package com.studyjavamaven.study01;
 
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class Exam001JavaTest {
@@ -24,13 +27,22 @@ public class Exam001JavaTest {
         assertThat(29).isLessThan(subTest001(100, 70));
         assertThat(301).isGreaterThan(subTest001(1000, 700));
 
+
+        //Exception 예외가 발생해야지만 정상 동작으로 인식
+        assertThatThrownBy(() -> subTest001(70000, 7))
+                .isInstanceOf(Exception.class);
+        // RuntimeException 예외가 발생해야지만 정상 동작으로 인식
+        Throwable exception = assertThrows(RuntimeException.class, ()-> {
+           subTest001(2330,50002);
+        });
+        System.out.println(exception.toString());
     }
     int subTest001(int n1, int n2) throws Exception {
         if( n1 < -50000 || n1 > 50000) {
             throw new Exception("num1 값은 -50000보다 크거나 50000보다 작아야 함.");
         }
         if( n2 < -50000 || n2 > 50000) {
-            throw new Exception("num2 값은 -50000보다 크거나 50000보다 작아야 함.");
+            throw new RuntimeException("num2 값은 -50000보다 크거나 50000보다 작아야 함.");
         }
         return n1 - n2;
     }
