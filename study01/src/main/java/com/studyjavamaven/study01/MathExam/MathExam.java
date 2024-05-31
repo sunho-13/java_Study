e3f3c4800277cd9bf017e8bcc0b190d09a705e60
sunho13 <sun710101@gmail.com>
2024-05-31 21:38:26 +0900

    Update MathExam.java

--- study01/src/main/java/com/studyjavamaven/study01/MathExam/MathExam.java
+++ study01/src/main/java/com/studyjavamaven/study01/MathExam/MathExam.java
@@ -10,4 +10,15 @@ public class MathExam {
         }
         return n1 - n2;
     }
+
+    public int mathTest002(int n1, int n2) throws Exception {
+        if( n1 < -0 || n1 > 100) {
+            throw new Exception("num1 값은 -1보다 크거나 101보다 작아야 함.");
+        }
+        if( n2 < -0 || n2 > 100) {
+            throw new RuntimeException("num2 값은 -1보다 크거나 101보다 작아야 함.");
+        }
+        return n1 * n2;
+    }
 }
+
