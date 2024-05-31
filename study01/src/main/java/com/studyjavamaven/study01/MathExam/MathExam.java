82912d02799f37f41f6bde1f737abe14760688e3
sunho13 <sun710101@gmail.com>
2024-05-31 21:51:34 +0900

    Update MathExam.java

--- study01/src/main/java/com/studyjavamaven/study01/MathExam/MathExam.java
+++ study01/src/main/java/com/studyjavamaven/study01/MathExam/MathExam.java
@@ -12,13 +12,39 @@ public class MathExam {
     }
 
     public int mathTest002(int n1, int n2) throws Exception {
-        if( n1 < -0 || n1 > 100) {
+        if( n1 < -1 || n1 > 100) {
             throw new Exception("num1 값은 -1보다 크거나 101보다 작아야 함.");
         }
-        if( n2 < -0 || n2 > 100) {
+        if( n2 < -1 || n2 > 100) {
             throw new RuntimeException("num2 값은 -1보다 크거나 101보다 작아야 함.");
         }
         return n1 * n2;
+    }
+
+    public int mathTest003(int n1, int n2) throws Exception {
+        if( n1 < 0 || n1 > 100) {
+            throw new Exception("num1 값은 0보다 크거나 101보다 작아야 함.");
+        }
+        if( n2 < 0 || n2 > 100) {
+            throw new RuntimeException("num2 값은 0보다 크거나 101보다 작아야 함.");
+        }
+        return n1 / n2;
+    }
+
+    public int mathTest004(int n1, int n2) throws Exception {
+        if( n1 < -1 || n1 > 10000) {
+            throw new Exception("num1 값은 -1보다 크거나 10001보다 작아야 함.");
+        }
+        if( n2 < -1 || n2 > 10000) {
+            throw new RuntimeException("num2 값은 -1보다 크거나 10001보다 작아야 함.");
+        }
+
+        if(n2 == n1){
+            return -1;
+        }else{
+            return 1;
+        }
+
     }
 }
 
