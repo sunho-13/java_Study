811844f98689537f85e2c52eca4c2f7ccd223c1b
sunho13 <sun710101@gmail.com>
2024-06-03 19:42:27 +0900

    Update Exam001JavaTest.java

--- study01/src/test/java/com/studyjavamaven/study01/Exam001JavaTest.java
+++ study01/src/test/java/com/studyjavamaven/study01/Exam001JavaTest.java
@@ -109,4 +109,26 @@ public class Exam001JavaTest {
         assertThatThrownBy(()  -> math.mathTest005(78, 180))
                 .isInstanceOf(Exception.class);
     }
+
+    @Test
+    public void exam120817() throws Exception {
+        MathExam math = new MathExam();
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        assertThat(5.5).isEqualTo((math.exam120817(numbers)));
+        assertThat(94.0).isEqualTo((math.exam120817(new int[] {89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99})));
+        assertThatThrownBy(() -> math.exam120817(new int[] {0, 1000, 2000}))
+                .isInstanceOf(Exception.class);
+        //assertThatThrownBy(() -> math.exam120817(new int[] {}))
+        //        .isInstanceOf(Exception.class);
+
+        Throwable exception = assertThrows(Exception.class, () -> {
+            math.exam120817(new int[] {0, 1000, 2000});
+        });
+        System.out.println(exception.toString());
+
+        exception = assertThrows(Exception.class, ()-> math.exam120817(new int[] {}));
+        System.out.println(exception.toString());
+
+    }
 }
