818937a56e64fce94b2df4b8491368740920424a
sunho-13 <sun001017@naver.com>
2024-06-13 21:11:46 +0900

    Create Account.java

---
+++ study01/src/main/java/practice/Account.java
@@ -0,0 +1,33 @@
+package practice;
+
+public class Account{
+    private String num;
+    private String name;
+    private int money;
+
+    public Account(String num, String name, int money){
+        this.num = num;
+        this.name = name;
+        this.money = money;
+    }
+
+    public String getNum() {
+        return num;
+    }
+    public void setNum(String num){
+        this.num = num;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getMoney(){
+        return money;
+    }
+    public void setMoney(int money){
+        this.money = money;
+    }
+
+}
