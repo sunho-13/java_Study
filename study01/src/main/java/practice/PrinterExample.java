e024029e4a65b80c40dff820d513969e14809fb8
sunho13 <sun710101@gmail.com>
2024-06-13 18:01:23 +0900

    Create PrinterExample.java

---
+++ study01/src/main/java/practice/PrinterExample.java
@@ -0,0 +1,29 @@
+package practice;
+
+public class PrinterExample {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        printer.println(10);
+        printer.println(true);
+        printer.println(5.7);
+        printer.println("홍길동");
+    }
+
+    static class Printer {
+        void println(int x) {
+            System.out.println(x);
+        }
+
+        void println(boolean x) {
+            System.out.println(x);
+        }
+
+        void println(double x) {
+            System.out.println(x);
+        }
+
+        void println(String x) {
+            System.out.println(x);
+        }
+    }
+}
