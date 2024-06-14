bbd8221297e8c2633a4e55dcaa4a20eef5b0e1f8
sunho-13 <sun001017@naver.com>
2024-06-14 22:16:21 +0900

    Create Account.java

---
+++ study01/src/main/java/teacher/models/Account.java
@@ -0,0 +1,24 @@
+package teacher.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Account {
+    private String name;
+    private String bankNumber;
+    private int current;
+
+    public Account(String name, String bankNumber, int current) {
+        this.name = name;
+        this.bankNumber = bankNumber;
+        this.current = current;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Account{name='%s', bankNumber='%s', current=%d}"
+            , this.name, this.bankNumber, this.current);
+    }
+}
