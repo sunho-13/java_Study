618ad4f6048b44b2f9de022dd7cff49486797799
sunho-13 <sun001017@naver.com>
2024-06-25 10:48:33 +0900

    Update AccountFileRepository.java

--- study01/src/main/java/demoJson/AccountFileRepository.java
+++ study01/src/main/java/demoJson/AccountFileRepository.java
@@ -5,24 +5,28 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class AccountFileRepository implements AccountRepository {
-    private static final String fileName = "data.txt";
+    private String fileName;
+
+    public AccountFileRepository(String filename) {
+        fileName = filename;
+    }
 
     @Override
     public void loadJson(List<Account> accountList) throws Exception {
         File file = new File(fileName);
-        if(file.exists())
-        {
-            BufferedReader inFile = new BufferedReader(new FileReader(file));
-            String sLine = null;
-            accountList.clear();
-            while( (sLine = inFile.readLine()) != null ) {
-                Account account = new Account();
-                String[] items = sLine.split(",");
-                account.setName( items[0] );
-                account.setBankNumber(items[1]);
-                account.setCurrent(Integer.parseInt(items[2]));
-                accountList.add(account);
-            }
+        if (!file.exists()) {
+            return; // 파일이 없을때 실행하면 예외 없도록 처리함
+        }
+        BufferedReader inFile = new BufferedReader(new FileReader(file));
+        String sLine = null;
+        accountList.clear();
+        while ((sLine = inFile.readLine()) != null) {
+            Account account = new Account();
+            String[] items = sLine.split(",");
+            account.setName(items[0]);
+            account.setBankNumber(items[1]);
+            account.setCurrent(Integer.parseInt(items[2]));
+            accountList.add(account);
         }
     }
 
@@ -31,7 +35,7 @@ public class AccountFileRepository implements AccountRepository {
         FileOutputStream fileOut = new FileOutputStream(fileName);
         OutputStreamWriter writer = new OutputStreamWriter(fileOut, StandardCharsets.UTF_8);
 
-        for ( Account account : accountList ) {
+        for (Account account : accountList) {
             String str = String.format("%s,%s,%d,\n"
                     , account.getName(), account.getBankNumber(), account.getCurrent());
             writer.write(str);
