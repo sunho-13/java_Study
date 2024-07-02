b156c0b97409c3e126391ddc2dfc81a1adf809b3
sunho-13 <sun001017@naver.com>
2024-07-02 17:18:54 +0900

    1

---
+++ study01/src/main/java/CodeUp/CodeUp1905.java
@@ -0,0 +1,22 @@
+/*
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class CodeUp1905 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+
+        System.out.println(sumNaturalNumber(i));
+    }
+
+    private static int sumNaturalNumber(int i) {
+        if (i == 1) {
+            return 1;
+        } else {
+            return i + sumNaturalNumber(i - 1);
+        }
+    }
+}
+*/
---
+++ study01/src/main/java/CodeUp/n1953.java
@@ -0,0 +1,15 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class n1953 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        
+
+
+    }
+}
